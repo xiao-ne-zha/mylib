eea4606531d00394b4dc7884716a841ea3bef9ef
张志胜 <zhangzhisheng@tydic.com>
2022-05-03 22:40:01 +0800

    调整工程去除clj

--- src/clj/org/tovictory/utils/base/ds/map.clj
+++
@@ -1,7 +0,0 @@
-(ns org.tovictory.utils.base.ds.map)
-
-(defn rename-keys
-  "使用(f k)对m中的所有key进行重命名"
-  [m f]
-  (when m
-    (into {} (map (fn [[k v]] [(f k) v]) m))))
--- src/clj/org/tovictory/utils/base/ds/seq.clj
+++
@@ -1,9 +0,0 @@
-(ns org.tovictory.utils.base.ds.seq
-  (:require [org.tovictory.utils.base.ds.map :as m]))
-
-(defn rename-keys
-  "对seq中的所有map元素使用(f k)进行key转换"
-  [s f]
-  (when s
-    (into (empty s)
-          (map #(if (instance? clojure.lang.IPersistentMap %) (m/rename-keys % f) %) s))))
